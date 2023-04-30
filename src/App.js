92ef593f13030a9688c423821b8a6791248ae18f
Bjay <jayanjay2018@gmail.com>
2023-04-30 18:17:37 +0300

    Added The Sidebar Components

--- src/App.js
+++ src/App.js
@@ -3,24 +3,44 @@ import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from "./beats";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const beats = new SpotifyWebApi();
 
 function App() {
-
-  const [token, setToken] = useState();
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
-
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      console.log("[token]", token);
       beats.setAccessToken(_token);
+      beats.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+      beats.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
+      beats.getPlaylist("37i9dQZF1E34Ucml4HHx1w").then((playlist) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: playlist,
+        });
+      });
     }
-
-    console.log("token", token);
   }, []);
 
   return (
